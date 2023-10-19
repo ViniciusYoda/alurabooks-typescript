0ce183039a79f83dec058eaeffd19b393ac1dbd5
Vinícius Yoda <vyoda4604@gmail.com>
2023-10-19 07:51:52 -0300

    sr

--- src/componentes/Banner/index.tsx
+++ src/componentes/Banner/index.tsx
@@ -8,12 +8,17 @@ interface BannerProps {
     children?: ReactNode
 }
 
-const Banner = ({ titulo, subtitulo, children } : BannerProps) => {
-    return (<section className="banner-principal" style={{ backgroundImage: `url(${fundo})`}}>
-        <h2>{titulo}</h2>
-        <h3>{subtitulo}</h3>
-        {children}
-    </section>)
+const Banner = ({ titulo, subtitulo, children }: BannerProps) => {
+    return (
+        <section 
+            className="banner-principal" 
+            style={{ backgroundImage: `url(${fundo})` }}
+        >
+            <h2>{titulo}</h2>
+            <h3>{subtitulo}</h3>
+            {children}
+        </section>
+    )
 }
 
 export default Banner
--- src/componentes/BarraNavegacao/index.tsx
+++ src/componentes/BarraNavegacao/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import BotaoNavegacao from "../BotaoNavegacao"
 import ModalCadastroUsuario from "../ModalCadastroUsuario"
 import ModalLoginUsuario from "../ModalLoginUsuario"
@@ -12,6 +12,8 @@ const BarraNavegacao = () => {
     const [modalCadastroAberta, setModalCadastroAberta] = useState(false)
     const [modalLoginAberta, setModalLoginAberta] = useState(false)
 
+    let navigate = useNavigate();
+
     const token = sessionStorage.getItem('token')
 
     const [usuarioEstaLogado, setUsuarioEstaLogado] = useState<boolean>(token != null)
@@ -21,81 +23,97 @@ const BarraNavegacao = () => {
         setUsuarioEstaLogado(true)
     }
 
-    return (<nav className="ab-navbar">
-        <h1 className="logo">
-            <Link to="/">
-                <img className="logo" src={logo} alt="Logo da AluraBooks" />
-            </Link>
-        </h1>
-        <ul className="navegacao">
-            <li>
-                <a href="#!">Categorias</a>
-                <ul className="submenu">
-                    <li>
-                        <Link to="/">
-                            Frontend
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Programação
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Infraestrutura
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Business
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Design e UX
-                        </Link>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <ul className="acoes">
-            {!usuarioEstaLogado && (<>
+    const efetuarLogout = () => {
+        setUsuarioEstaLogado(false)
+        sessionStorage.removeItem('token')
+        navigate('/')
+    }
+
+    return (
+        <nav className="ab-navbar">
+            <h1 className="logo">
+                <Link to="/">
+                    <img className="logo" src={logo} alt="Logo da AluraBooks" />
+                </Link>
+            </h1>
+            <ul className="navegacao">
                 <li>
-                    <BotaoNavegacao
-                        texto="Login"
-                        textoAltSrc="Icone representando um usuário"
-                        imagemSrc={usuario}
-                        onClick={() => setModalLoginAberta(true)}
-                    />
-                    <ModalLoginUsuario
-                        aberta={modalLoginAberta}
-                        aoFechar={() => setModalLoginAberta(false)}
-                        aoEfetuarLogin={aoEfetuarLogin}
-                    />
+                    <a href="#!">Categorias</a>
+                    <ul className="submenu">
+                        <li>
+                            <Link to="/">
+                                Frontend
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                Programação
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                Infraestrutura
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                Business
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                Design e UX
+                            </Link>
+                        </li>
+                    </ul>
                 </li>
-                <li>
-                    <BotaoNavegacao
-                        texto="Cadastrar-se"
-                        textoAltSrc="Icone representando um usuário"
-                        imagemSrc={usuario}
-                        onClick={() => setModalCadastroAberta(true)}
-                    />
-                    <ModalCadastroUsuario
-                        aberta={modalCadastroAberta}
-                        aoFechar={() => setModalCadastroAberta(false)}
-                    />
-                </li>
-            </>)}
-            {usuarioEstaLogado &&
-                <>
+            </ul>
+            <ul className="acoes">
+                {!usuarioEstaLogado && (<>
                     <li>
-                        <Link to="/minha-conta/pedidos">Minha conta</Link>
+                        <BotaoNavegacao
+                            texto="Login"
+                            textoAltSrc="Icone representando um usuário"
+                            imagemSrc={usuario}
+                            onClick={() => setModalLoginAberta(true)}
+                        />
+                        <ModalLoginUsuario
+                            aberta={modalLoginAberta}
+                            aoFechar={() => setModalLoginAberta(false)}
+                            aoEfetuarLogin={aoEfetuarLogin}
+                        />
                     </li>
-                </>
-            }
-        </ul>
-    </nav>)
+                    <li>
+                        <BotaoNavegacao
+                            texto="Cadastrar-se"
+                            textoAltSrc="Icone representando um usuário"
+                            imagemSrc={usuario}
+                            onClick={() => setModalCadastroAberta(true)}
+                        />
+                        <ModalCadastroUsuario
+                            aberta={modalCadastroAberta}
+                            aoFechar={() => setModalCadastroAberta(false)}
+                        />
+                    </li>
+                </>)}
+                {usuarioEstaLogado &&
+                    <>
+                        <li>
+                            <Link to="/minha-conta/pedidos">Minha conta</Link>
+                        </li>
+                        <li>
+                            <BotaoNavegacao
+                                texto="Logout"
+                                textoAltSrc="Icone representando um usuário"
+                                imagemSrc={usuario}
+                                onClick={efetuarLogout}
+                            />
+                        </li>
+                    </>
+                }
+            </ul>
+        </nav>
+    )
 }
 
 export default BarraNavegacao
